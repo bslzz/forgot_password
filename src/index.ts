0c21b9845dea823fddd63cf8bb89d8505d07ba5c
bslzz <bishalkoid@gmail.com>
2021-12-11 00:36:51 +0200

    enabled cors

--- src/index.ts
+++ src/index.ts
@@ -16,10 +16,23 @@ const app = express()
 // middleware
 app.use(express.json())
 app.use(cookieParser())
-app.use(cors())
+app.use(
+  cors({
+    origin: 'http://localhost:3000'
+  })
+)
 
 // connect db
 CONNECT_DB()
+
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  )
+  next()
+})
 
 // routes
 app.use('/users', userRoute)
